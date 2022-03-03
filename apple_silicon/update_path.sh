96bd74159b2b8aa263149441423d437ec190b59c
zhixian001 <okyjh12@naver.com>
2022-03-03 15:59:47 +0900

    format code

--- apple_silicon/update_path.sh
+++ apple_silicon/update_path.sh
@@ -10,12 +10,20 @@ if [[ $current_arch == "i386" ]]; then
     # See: https://unix.stackexchange.com/questions/108873/removing-a-directory-from-path
     TMP_PATH="/usr/local/opt/llvm/bin:${PATH}"
 
-    export PATH=`echo $TMP_PATH | tr ":" "\n" | grep -v "/opt/homebrew/bin" | grep -v "/opt/homebrew/sbin" | grep -v "/opt/homebrew/opt/llvm/bin" | tr "\n" ":"`
+    export PATH=`echo $TMP_PATH | tr ":" "\n" | \
+            grep -v "/opt/homebrew/bin" | \
+            grep -v "/opt/homebrew/sbin" | \
+            grep -v "/opt/homebrew/opt/llvm/bin" | \
+        tr "\n" ":"`
 
 else
     TMP_PATH="/opt/homebrew/opt/llvm/bin:${PATH}"
 
-    export PATH=`echo $TMP_PATH | tr ":" "\n" | grep -v "/opt/homebrew/bin" | grep -v "/opt/homebrew/sbin" | grep -v "/usr/local/opt/llvm/bin" | tr "\n" ":"`
+    export PATH=`echo $TMP_PATH | tr ":" "\n" | \
+            grep -v "/opt/homebrew/bin" | \
+            grep -v "/opt/homebrew/sbin" | \
+            grep -v "/usr/local/opt/llvm/bin" | \
+        tr "\n" ":"`
 
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
