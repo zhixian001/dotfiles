e93e0fb3f5d01c168fade05b8de14bbdaf63ea4d
zhixian001 <okyjh12@naver.com>
2022-03-03 15:48:59 +0900

    move m1 env scripts to new file

---
+++ apple_silicon/update_path.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# M1 switch shell architecture
+alias arm="env /usr/bin/arch -arm64 /opt/homebrew/bin/bash"
+alias intel="env /usr/bin/arch -x86_64 /usr/local/bin/bash"
+
+
+current_arch="$(arch)"
+if [[ $current_arch == "i386" ]]; then
+    # See: https://unix.stackexchange.com/questions/108873/removing-a-directory-from-path
+    TMP_PATH="/usr/local/opt/llvm/bin:${PATH}"
+
+    export PATH=`echo $TMP_PATH | tr ":" "\n" | grep -v "/opt/homebrew/bin" | grep -v "/opt/homebrew/sbin" | grep -v "/opt/homebrew/opt/llvm/bin" | tr "\n" ":"`
+
+else
+    TMP_PATH="/opt/homebrew/opt/llvm/bin:${PATH}"
+
+    export PATH=`echo $TMP_PATH | tr ":" "\n" | grep -v "/opt/homebrew/bin" | grep -v "/opt/homebrew/sbin" | grep -v "/usr/local/opt/llvm/bin" | tr "\n" ":"`
+
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
