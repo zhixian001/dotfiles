3d50fe79667d4d0ff6e35851e60311c00f356c1c
zhixian001 <okyjh12@naver.com>
2022-03-12 21:09:45 +0900

    fix update nvm

--- macos/apple_silicon/update_nvm.sh
+++ macos/apple_silicon/update_nvm.sh
@@ -9,16 +9,13 @@ __remove_from_path "nvm"
 current_arch="$(arch)"
 if [[ $current_arch == "i386" ]]; then
     export NVM_DIR="$JH_X86_NVM_DIR"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 else
     if [[ -r "$JH_ARM_NVM_DIR" ]]; then
         export NVM_DIR="$JH_ARM_NVM_DIR"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     else
         export NVM_DIR="$JH_X86_NVM_DIR"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     fi
 fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
