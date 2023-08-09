3d149327c7cf5a42ca96a9415d41952286c49e25
zhixian001 <okyjh12@naver.com>
2023-08-09 14:20:46 +0900

    add fzf-man custom command

--- bashrc-extensions/fzf.sh
+++ bashrc-extensions/fzf.sh
@@ -55,4 +55,14 @@ _fzf_comprun() {
   esac
 }
 
+function fzf-man(){
+    MAN="/usr/bin/man"
+    if [ -n "$1" ]; then
+        $MAN "$@"
+        return $?
+    else
+        $MAN -k . 2> /dev/null | fzf --reverse --preview="echo {1,2} | sed 's/ (/./' | sed -E 's/\)\s*$//' | xargs $MAN" | awk '{print $1 "." $2}' | tr -d '()' | xargs -r $MAN
+        return $?
+    fi
+}
 
