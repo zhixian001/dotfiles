463f9bade13f290a05ca3de96586dea61a312d15
zhixian001 <okyjh12@naver.com>
2024-01-27 00:19:34 +0900

    add gls fallback

--- bashrc/common/aliases/gls.alias.sh
+++ bashrc/common/aliases/gls.alias.sh
@@ -1,3 +1,10 @@
+__register_aliases_if_command_exist ls \
+    "l='ls --color=auto'" \
+    "ls='ls --color=auto'" \
+    "ll='ls -l --color=auto'" \
+    "la='ls -a --color=auto'" \
+    "lla='ls -al --color=auto'"
+
 __register_aliases_if_command_exist gls \
     "l='gls --color'" \
     "ls='gls --color'" \
